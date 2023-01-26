3b759a6aeed2fbe54d7fb6fee518e05c45f357d1
edinhaurdic <8511Edin>
2023-01-26 19:25:09 +0100

    "green and blue fase in test to create AppUser"

--- src/main/java/org/example/AppUser.java
+++ src/main/java/org/example/AppUser.java
@@ -1,4 +1,35 @@
 package org.example;
 
+import java.util.Objects;
+
 public class AppUser {
+
+    private String username;
+    private String password;
+
+    public AppUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AppUser)) return false;
+        AppUser appUser = (AppUser) o;
+        return getUsername().equals(appUser.getUsername());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername());
+    }
 }
--- src/test/java/org/example/AppUserTest.java
+++ src/test/java/org/example/AppUserTest.java
@@ -7,12 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AppUserTest {
 
     @Test
-    public void constructor_withoutProp_ShouldExist(){
+    public void create_newAppUser_shouldCreateAppUser(){
         //given
         AppUser appUser;
 
         //When
-        appUser = new AppUser();
+        appUser = new AppUser("Edin", "1234hej");
 
         //Then
         assertNotNull(appUser);
